256bde0b9162573264f04e097404fb10ca96b843
Immanuel Salva <83900884+ImmanuelSalva@users.noreply.github.com>
2021-08-24 10:37:04 +0700

    Update app.js

--- server/app.js
+++ server/app.js
@@ -2,6 +2,33 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
+// add after other require() statements
+
+const cors = require('cors');
+
+// add after require() statements
+
+const videos = [
+    {
+        id: 0,
+        poster: '/video/0/poster',
+        duration: '3 mins',
+        name: 'Sample 1'
+    },
+    {
+        id: 1,
+        poster: '/video/1/poster',
+        duration: '4 mins',
+        name: 'Sample 2'
+    },
+    {
+        id: 2,
+        poster: '/video/2/poster',
+        duration: '2 mins',
+        name: 'Sample 3'
+    },
+];
+
 const app = express();
 
 // add after 'const app = express();'
@@ -9,6 +36,11 @@ const app = express();
 app.get('/video', (req, res) => {
     res.sendFile('assets/sample.mp4', { root: __dirname });
 });
+
+// add after existing app.get('/video', ...) route
+
+app.use(cors());
+app.get('/videos', (req, res) => res.json(videos));
 
 // add to end of file
 
